c51cdbe065de2767ec72cbcc500497ac68523b26
niveK77pur <KevinBiewesch@yahoo.fr>
2019-04-23 18:59:04 +0200

    Small fix to scripts/provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -3,7 +3,7 @@
 # $1 : instance name
 # $2 : user name at the instance
 
-HOST_FILE="../hosts"
+HOST_FILE=$(mktemp --suffix=.hosts)
 PLAYBOOK="../playbook.yml"
 
 # Make sure Ansible is installed
@@ -12,10 +12,8 @@ PLAYBOOK="../playbook.yml"
 provision-instance(){
         IP=$(gcloud compute instances list --format="csv(name,networkInterfaces[0].accessConfigs[0].natIP)" | grep "^$1," | cut -d"," -f2)
         [ -z "$IP" ] && { echo "No IP found for '$1'"; exit 2; }
-        echo "$IP ansible_user=$2" >> "$HOST_FILE"
+        echo "$IP ansible_user=$2" #>> "$HOST_FILE"
         ansible-playbook -i "$HOST_FILE" "$PLAYBOOK"
-        echo 
-        echo "NOTE: To provision all the files run $0 -a"
 }
 
 provision-all(){
@@ -29,7 +27,7 @@ provision-all(){
                         echo "${host[0]} has no external ip. It is probably not running."
                 else
                         echo "Provisioning ${host[0]} ..."
-                        ansible-playbook -i "$HOST_FILE" "$PLAYBOOK"
+                        provision-instance "${host[0]}" "$1"
                 fi
         done < "$TMP_FILE"
         rm "$TMP_FILE"
@@ -40,11 +38,13 @@ if [ $# -ne 1 ] && [ $# -ne 2 ]
 then
         echo "Invalid parameters for $0"
         echo "Use either of these two"
-        echo "  $0 -a"
-        echo "  $0 INSTANCE_NAME USER_NAME"
-elif [ "$1" = "-a" ] && [ $# -eq 1 ]
+        echo "  $0 -a USER_NAME                 Provision all running instances"
+        echo "  $0 INSTANCE_NAME USER_NAME      Provision specific instance"
+elif [ "$1" = "-a" ] 
 then
-        provision-all
+        provision-all "$2"
 else
         provision-instance "$1" "$2"
+        echo 
+        echo "NOTE: To provision all the files run $0 -a"
 fi
